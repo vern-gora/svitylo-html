c9a84b4260eeb8e87ca310fde2aa18903708240d
Danylo <danvern99@gmail.com>
2024-02-27 23:36:12 +0200

    add header css

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
-// function component() {
-//   const element = document.createElement('div');
+document.addEventListener("DOMContentLoaded", function () {
+  // Отримуємо посилання на всі кнопки у хедері
+  var headerItems = document.querySelectorAll(".a-btn");
 
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(['Hello', 'webpack', '122'], ' ');
+  // Встановлюємо клас "active" на перший елемент за замовчуванням
+  headerItems[0].classList.add("active");
 
-//   return element;
-// }
+  // Додаємо слухача подій для кожної кнопки
+  headerItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Видаляємо клас "active" з усіх елементів
+      headerItems.forEach(function (el) {
+        el.classList.remove("active");
+      });
 
-// document.body.appendChild(component());
+      // Додаємо клас "active" до натисканого елемента
+      item.classList.add("active");
+    });
+  });
+});
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
